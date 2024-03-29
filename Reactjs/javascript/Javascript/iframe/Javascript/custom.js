9e9c6d3e8000d743ea3cc96068d65fed760ee18b
aksharchaudhary4119 <aksharchaudhary773@gmail.com>
2024-03-29 11:25:57 +0530

    iframe data

---
+++ Reactjs/javascript/Javascript/iframe/Javascript/custom.js
@@ -0,0 +1,6 @@
+function hitme() {
+    alert("Btn Click Success!!!")
+}
+document.getElementById("btn").addEventListener("click", function () {
+    alert("Btn Click addEventListener Success!!!")
+})
