5a1deab9e5200febf5a01f9f123e0234ca1625bf
aksharchaudhary4119 <aksharchaudhary773@gmail.com>
2024-05-31 12:26:59 +0530

    data

---
+++ React/my-app/src/app/Header.js
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return(
+        <main className="Header">
+           import {  } from "./logo.js";
+           import {  } from "./search.js";
+
+       </main> 
+    )
+    
+}
