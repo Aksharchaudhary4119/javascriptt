e4eb5a13ed0db728d2c1059eca18242a84bee101
aksharchaudhary4119 <aksharchaudhary773@gmail.com>
2024-07-01 12:33:35 +0530

    data

--- React/practical-app/src/Example/Functionalcomponents/19ChildComponent.jsx
+++ React/practical-app/src/Example/Functionalcomponents/19ChildComponent.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 
 const ChildComponent = (props) => {
-    let [ state,setState ] = useState("data");
+    let [ state ,setState ] = useState("data");
     return (
-        <>
+        <div>
             {props.title}
             {<input type='text' onChange={(event) => {props.func (event.target.value)}} name='' id=''/>}
             {<input type='text' onChange={(e) => { setState (e.target.value)}} name='' id=''/>}
             {<input type='text' onChange={(e) => {props.func (e.target.value)}} name='' id=''/>}
-        </>
+            {state}
+        </div>
     );
 };
 
